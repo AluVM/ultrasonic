68da93f5d0ade987031fbade64d3dfca4115ceb4
Crayon Shin-chan <xiaoxin@bitlightlabs.com>
2025-04-08 15:30:36 +0800

    chore: derive AsSlice

--- src/contract.rs
+++ src/contract.rs
@@ -149,7 +149,7 @@ pub enum ContractName {
 
 /// Unique contract identifier equivalent to the contract genesis commitment
 #[derive(Wrapper, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, From)]
-#[wrapper(Deref, BorrowSlice, Hex, Index, RangeOps)]
+#[wrapper(AsSlice, Deref, BorrowSlice, Hex, Index, RangeOps)]
 #[derive(StrictType, StrictDumb, StrictEncode, StrictDecode)]
 #[strict_type(lib = LIB_NAME_ULTRASONIC)]
 #[cfg_attr(
