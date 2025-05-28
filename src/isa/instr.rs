90605f55cc47c95dbc3ee4fc014adb5c6e0c73b3
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2025-05-28 10:22:36 +0200

    doc: improve spelling

--- src/isa/instr.rs
+++ src/isa/instr.rs
@@ -67,8 +67,9 @@ impl<Id: SiteId> From<aluvm::gfa::Instr<Id>> for Instr<Id> {
 
 /// The instruction set uses iterator semantics and not random access semantic to correspond to the
 /// RISC type of the machine and not to add assumptions about abilities to access the operation
-/// state in a random way. Operation state is always iterated, such that not a single state
-/// element can be missed (as long as iterator runs to the end).
+/// state randomly.
+/// Operation state is always iterated, such that not a single state element can be missed (as long
+/// as the iterator runs to the end).
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug, Display)]
 #[display(inner)]
 #[non_exhaustive]
@@ -93,57 +94,57 @@ pub enum UsonicInstr {
     #[display("cknxo   :immutable")]
     CkNxOAo,
 
-    /// Load next [`StateValue`] from the current input destructible memory cell to `EA`-`ED`
+    /// Load next [`StateValue`] from the current destructible memory cell input to `EA`-`ED`
     /// registers.
     ///
     /// If the next state value is absent, sets `CO` to a failed state. Otherwise, resets `CO`.
     #[display("ldi     :destructible")]
     LdIRo,
 
-    /// Load next [`StateValue`] from the current input immutable memory cell to `EA`-`ED`
+    /// Load next [`StateValue`] from the current immutable memory cell input to `EA`-`ED`
     /// registers.
     ///
     /// If the next state value is absent, sets `CO` to a failed state. Otherwise, resets `CO`.
     #[display("ldi     :immutable")]
     LdIAo,
 
-    /// Load next [`StateValue`] from the current output destructible memory cell to `EA`-`ED`
+    /// Load next [`StateValue`] from the current destructible memory cell output to `EA`-`ED`
     /// registers.
     ///
     /// If the next state value is absent, sets `CO` to a failed state. Otherwise, resets `CO`.
     #[display("ldo     :destructible")]
     LdORo,
 
-    /// Load next [`StateValue`] from the current output immutable memory cell to `EA`-`ED`
+    /// Load next [`StateValue`] from the current immutable memory cell output to `EA`-`ED`
     /// registers.
     ///
     /// If the next state value is absent, sets `CO` to a failed state. Otherwise, resets `CO`.
     #[display("ldo     :immutable")]
     LdOAo,
 
-    /// Resets iterator over input destructible memory cells by setting corresponding `UI` value to
-    /// zero.
+    /// Resets iterator over the input destructible memory cells by setting the corresponding `UI`
+    /// value to zero.
     ///
     /// Does not affect the value of `CO` or `CK` registers.
     #[display("rsti    :destructible")]
     RstIRo,
 
-    /// Resets iterator over input immutable memory cells by setting corresponding `UI` value to
-    /// zero.
+    /// Resets iterator over the input immutable memory cells by setting the corresponding `UI`
+    /// value to zero.
     ///
     /// Does not affect the value of `CO` or `CK` registers.
     #[display("rsti    :immutable")]
     RstIAo,
 
-    /// Resets iterator over output destructible memory cells by setting corresponding `UI` value
-    /// to zero.
+    /// Resets iterator over the output destructible memory cells by setting the corresponding `UI`
+    /// value to zero.
     ///
     /// Does not affect the value of `CO` or `CK` registers.
     #[display("rsto    :destructible")]
     RstORo,
 
-    /// Resets iterator over output immutable memory cells by setting corresponding `UI` value to
-    /// zero.
+    /// Resets iterator over the output immutable memory cells by setting the corresponding `UI`
+    /// value to zero.
     ///
     /// Does not affect the value of `CO` or `CK` registers.
     #[display("rsto    :immutable")]
